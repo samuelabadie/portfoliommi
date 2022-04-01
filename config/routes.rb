6c2ddf9157260325bbf01a657b11ce2c3b1bc948
samuelabadie <abadiesamuel10@gmail.com>
2022-04-01 18:26:29 +0200

    login nregistration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
   root to:'home#index'
+  devise_for :uusers
 
+  resources :uusers
   resources :ateliers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
